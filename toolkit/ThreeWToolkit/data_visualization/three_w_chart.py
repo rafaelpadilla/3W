096a0114905f66b52d5684d143fd43d07f179cc2
Mathtzt <matheus.cn10@gmail.com>
2025-11-24 17:51:03 -0300

    Format code with Black.

--- toolkit/ThreeWToolkit/data_visualization/three_w_chart.py
+++ toolkit/ThreeWToolkit/data_visualization/three_w_chart.py
@@ -219,9 +219,9 @@ class ThreeWChart:
 
             # Loads data from the Parquet file
             df = pd.read_parquet(fp, engine="pyarrow")
-            assert (df.columns == self.dataset_ini["COLUMNS_DATA_FILES"][1:]).all(), (
-                f"invalid columns in the file {fp}: {df.columns.tolist()}"
-            )
+            assert (
+                df.columns == self.dataset_ini["COLUMNS_DATA_FILES"][1:]
+            ).all(), f"invalid columns in the file {fp}: {df.columns.tolist()}"
         except Exception as e:
             raise Exception(f"error reading file {fp}: {e}")
 
